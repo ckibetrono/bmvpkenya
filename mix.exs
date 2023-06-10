b3481313cda809993701ad7f28cfbf5ad73000b6
ckibetrono <ckibetrono@gmail.com>
2023-06-10 17:05:29 +0300

    Add hackney

--- mix.exs
+++ mix.exs
@@ -45,6 +45,7 @@ defmodule Bmvpkenya.MixProject do
       {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
+      {:hackney, "~> 1.18"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
